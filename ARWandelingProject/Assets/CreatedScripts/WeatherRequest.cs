2df6c67e25b958492ed4660e4f839333ca554fe5
Githubjael <jaelimamdi@gmail.com>
2021-09-28 15:55:11 +0200

    Update WeatherRequest.cs

--- ARWandelingProject/Assets/CreatedScripts/WeatherRequest.cs
+++ ARWandelingProject/Assets/CreatedScripts/WeatherRequest.cs
@@ -1,18 +1,35 @@
+using Newtonsoft.Json;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class WeatherRequest : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private string url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&types=food&name=harbour&key=AIzaSyAueLH6XEvQUexoYz83mh7ZXbe1FEPL7JQ";
+
+    Text Places;
+
+    public void GetWeather()
     {
-        
+        StartCoroutine(MakeWeatherRequest());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator MakeWeatherRequest()
     {
-        
+        UnityWebRequest request = UnityWebRequest.Get(url);
+        yield return request.SendWebRequest();
+
+        if(request.isNetworkError || request.isHttpError)
+        {
+            Debug.LogError(request.error);
+        }
+        else
+        {
+            var weather = JsonConvert.DeserializeObject<WeatherResponse>(request.downloadHandler.text);
+
+            var t = weather;
+        }
     }
 }
