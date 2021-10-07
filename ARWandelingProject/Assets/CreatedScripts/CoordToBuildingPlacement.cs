2b558194d3be379c34a17c5d1facc5f27e0590d2
Githubjael <jaelimamdi@gmail.com>
2021-10-07 11:50:30 +0200

    AAAAAaaaahahahahAAHAhahahAAaAAHHa mille

---
+++ ARWandelingProject/Assets/CreatedScripts/CoordToBuildingPlacement.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoordToBuildingPlacement : MonoBehaviour
+{
+    public float longitude;
+    public float latitude;
+    [SerializeField]
+    public List<GameObject> Buildings;
+}
