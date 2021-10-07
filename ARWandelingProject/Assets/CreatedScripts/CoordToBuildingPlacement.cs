aa8e5e7e01906e9b8d482e1a5b6c455e3ce710d0
Githubjael <jaelimamdi@gmail.com>
2021-10-07 14:08:31 +0200

    Scripting

--- ARWandelingProject/Assets/CreatedScripts/CoordToBuildingPlacement.cs
+++ ARWandelingProject/Assets/CreatedScripts/CoordToBuildingPlacement.cs
@@ -7,21 +7,35 @@ public class CoordToBuildingPlacement : MonoBehaviour
 {
     public float longitude;
     public float latitude;
-    private float latScale;
-    private float lonScale;
-    [SerializeField]
-    private List<GameObject> Buildings;
 
-    public void Start()
+    public double AnchorpointA;
+    public double AnchorpointB;
+
+    public List<GameObject> Buildings;
+
+    public void GetGPSData()
     {
-        GeoCoordsToUnityCoords();
+        var currentGPSposition = Input.location.lastData;
+        float z = latToZ(Input.location.lastData.latitude);
+        float x = lonToX(Input.location.lastData.longitude);
+
+        this.transform.position = new Vector3(x, 0f, z);
+
     }
 
-    private void GeoCoordsToUnityCoords()
+    float latToZ(double latitude)
     {
-        latScale = gameObject.transform.position.y / 180;
-        latScale = gameObject.transform.position.x / 360;
+        latitude = (latitude - AnchorpointA) / 0.00001 * 00.12179047095976932582726898256213;
+        double z = latitude;
 
-
+        return (float)z;
     }
+    float lonToX(double longitude)
+    {
+        longitude = (longitude - AnchorpointB) / 0.00001 * 00.00728553580298947812081345114627;
+        double x = longitude;
+
+        return (float)x ;
+    }
+
 }
