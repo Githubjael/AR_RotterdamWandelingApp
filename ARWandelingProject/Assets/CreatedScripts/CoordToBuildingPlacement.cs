3644e87e2d11c2c427bdd8120ac072f7f54fa84f
Githubjael <jaelimamdi@gmail.com>
2021-10-07 12:23:24 +0200

    Update CoordToBuildingPlacement.cs

--- ARWandelingProject/Assets/CreatedScripts/CoordToBuildingPlacement.cs
+++ ARWandelingProject/Assets/CreatedScripts/CoordToBuildingPlacement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,21 @@ public class CoordToBuildingPlacement : MonoBehaviour
 {
     public float longitude;
     public float latitude;
+    private float latScale;
+    private float lonScale;
     [SerializeField]
     private List<GameObject> Buildings;
+
+    public void Start()
+    {
+        GeoCoordsToUnityCoords();
+    }
+
+    private void GeoCoordsToUnityCoords()
+    {
+        latScale = gameObject.transform.position.y / 180;
+        latScale = gameObject.transform.position.x / 360;
+
+
+    }
 }
