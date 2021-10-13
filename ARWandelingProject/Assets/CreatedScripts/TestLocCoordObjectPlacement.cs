53b54c5cf62a8cd180a996c91234a760afd9350b
Githubjael <jaelimamdi@gmail.com>
2021-10-13 09:56:13 +0200

    bad behaveiour
    Wat would take their place

--- ARWandelingProject/Assets/CreatedScripts/TestLocCoordObjectPlacement.cs
+++ ARWandelingProject/Assets/CreatedScripts/TestLocCoordObjectPlacement.cs
@@ -33,7 +33,7 @@ public class TestLocCoordObjectPlacement : MonoBehaviour
         Instantiate(Buildings[0] , new Vector3(x , 0f ,z), Quaternion.identity);
         z = LatsToZ(Input.location.lastData.latitude, Input.location.lastData.latitude);
         x = LonsToX(Input.location.lastData.latitude, Input.location.lastData.longitude);
-        Instantiate(Buildings[0], new Vector3(x - Offset[0], 0f, z), Quaternion.identity);
+        Instantiate(Buildings[0], new Vector3(x , 0f, z), Quaternion.identity);
     }
     #endregion
     private void Start()
