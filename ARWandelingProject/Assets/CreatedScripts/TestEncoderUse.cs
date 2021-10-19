f107e2a99b4b097d8b71dd637be89252ac8bd888
Githubjael <jaelimamdi@gmail.com>
2021-10-19 11:22:39 +0200

    Look a lot happend
    In the last few minutes, okay.
    Gimme a break.

--- ARWandelingProject/Assets/CreatedScripts/TestEncoderUse.cs
+++ ARWandelingProject/Assets/CreatedScripts/TestEncoderUse.cs
@@ -3,13 +3,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Android;
+using TMPro;
 
 public class TestEncoderUse : MonoBehaviour
 {
 
     public static GPSEncoder GPSEncoder;
     public bool IsUpdating;
-
+    public TextMeshProUGUI Status;
 
     public void Update()
     {
@@ -20,7 +21,7 @@ public class TestEncoderUse : MonoBehaviour
     private IEnumerator GetGPS()
     {
 
-        int maxWait = 3;
+        int maxWait = 5;
         if (!Permission.HasUserAuthorizedPermission(Permission.FineLocation)|| !Permission.HasUserAuthorizedPermission(Permission.CoarseLocation))
         {
             Permission.RequestUserPermission(Permission.FineLocation);
@@ -33,8 +34,12 @@ public class TestEncoderUse : MonoBehaviour
             yield return new WaitForSeconds(5);
         }
 
+        //start location service before querying
+        Input.location.Start();
+
         while(Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
         {
+            Status.text = "Initializing...";
             yield return new WaitForSeconds(1);
             maxWait--;
         }
@@ -42,16 +47,21 @@ public class TestEncoderUse : MonoBehaviour
         //if location service doesn't initialize n 20 secs
         if(maxWait > 1)
         {
+            Status.text += "Timed Out!";
             yield break;
         }
 
         //Connection failed
         if(Input.location.status == LocationServiceStatus.Failed)
         {
-
+            Status.text = "Unable to determine device location.";
         }
-
+        else
+        {
+            Status.text = $"Latitude: {Input.location.lastData.latitude}" + " " + $"Longitude: {Input.location.lastData.longitude}";
+        }
         IsUpdating = !IsUpdating;
         Input.location.Stop();
+
     }
 }
