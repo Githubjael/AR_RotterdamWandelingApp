dbe9afa4fae41d1b81700f49eedcb3d673cbff9b
Githubjael <jaelimamdi@gmail.com>
2021-10-20 15:21:19 +0200

    Only the dead are biodegradeable.

--- ARWandelingProject/Assets/CreatedScripts/TestEncoderUse.cs
+++ ARWandelingProject/Assets/CreatedScripts/TestEncoderUse.cs
@@ -12,7 +12,8 @@ public class TestEncoderUse : MonoBehaviour
     public bool IsUpdating;
     public TextMeshProUGUI Status;
     public TextMeshProUGUI Coords;
-    public TextMeshProUGUI CurrentCoord;
+    public TextMeshProUGUI CurrentCoordingame;
+    public TextMeshProUGUI CurrentCoordsIRL;
     public List<GameObject> BuildingsToPlace;
     
     public void Awake()
@@ -29,24 +30,69 @@ public class TestEncoderUse : MonoBehaviour
     {
         if (!IsUpdating)
         {
-            GetGPSEncoder();
+            StartCoroutine(GetGPSEncoder());
             StartCoroutine(GetGPS());
             IsUpdating = !IsUpdating;
         }
     }
 
     #region GetGPSEncoder
-    public void GetGPSEncoder()
+    public IEnumerator GetGPSEncoder()
     {
-        double latitude = 51.923460;
-        double longitude = 4.481160;
+        int Maxwait = 10;
+
+        double latitude = 51.92766449684264;
+        double longitude = 4.480420461816436;
+
         float lat = Convert.ToSingle(latitude);
         float lon = Convert.ToSingle(longitude);
+
+        if (!Permission.HasUserAuthorizedPermission(Permission.FineLocation) || !Permission.HasUserAuthorizedPermission(Permission.CoarseLocation))
+        {
+            Permission.RequestUserPermission(Permission.FineLocation);
+            Permission.RequestUserPermission(Permission.CoarseLocation);
+        }
+
+        if (Input.location.isEnabledByUser)
+        {
+            yield return new WaitForSeconds(5);
+        }
+        else
+        {
+            CurrentCoordsIRL.text = "Location is not enabled!";
+            yield break;
+        }
+
+        Input.location.Start();
+
+        while(Input.location.status == LocationServiceStatus.Initializing && Maxwait >= 0)
+        {
+            yield return new WaitForSeconds(1);
+            Maxwait--;
+        }
+
+        if(Maxwait < 0)
+        {
+            CurrentCoordsIRL.text = "Timed Out!";
+            yield break;
+        }
+
+        if(Input.location.status == LocationServiceStatus.Failed)
+        {
+            CurrentCoordsIRL.text = "Unable to determin device location";
+            yield break;
+        }
+        else
+        {
+            double irlLatitude = Input.location.lastData.latitude;
+            double irlLongitude = Input.location.lastData.longitude;
+            CurrentCoordsIRL.text = GPSEncoder.GPSToUCS( (float)irlLatitude, (float)irlLongitude).ToString();
+        }
 
         Vector3 currentcoordposition = GPSEncoder.USCToGPS(CurrentARCameraCoordPosition.transform.position);
         Vector3 coordinates = GPSEncoder.GPSToUCS(new Vector2(lat, lon));
 
-        CurrentCoord.text = $"{currentcoordposition}";
+        CurrentCoordingame.text = $"{currentcoordposition}";
 
         Coords.text = coordinates.ToString();
 
@@ -61,6 +107,9 @@ public class TestEncoderUse : MonoBehaviour
         /*string latcoord = Input.location.lastData.latitude.ToString();
         string loncoord = Input.location.lastData.longitude.ToString();
         CurrentCoord.text = $"{CurrenCoordtPosition}" + $"{latcoord}" + $"{loncoord}";*/
+        Input.location.Stop();
+        IsUpdating = !IsUpdating;
+
     }
     #endregion
 
