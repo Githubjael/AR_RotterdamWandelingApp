f9ece69c8ef3b97fc55043bfa826f97a63209be1
Githubjael <jaelimamdi@gmail.com>
2021-10-25 10:14:44 +0200

    Cheesecake
    gonna test a build might be decent.

--- ARWandelingProject/Assets/CreatedScripts/NewTestCoords.cs
+++ ARWandelingProject/Assets/CreatedScripts/NewTestCoords.cs
@@ -1,13 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Android;
 using UnityEngine;
+using TMPro;
 
 public class NewTestCoords : MonoBehaviour
 {
-    // Start is called before the first frame update
+    #region PlaceholderTitle
+    public List<GameObject> ObjectPlacement;
+
+    public Vector2 LocalOrigin = new Vector2((float)51.92440614616623, (float)4.477705312843138);
+
+    public TextMeshProUGUI Loc1Text;
+    public TextMeshProUGUI CurrentCoordText;
+    #endregion
+
+    private void Awake()
+    {
+        GPSEncoder.SetLocalOrigin(LocalOrigin);
+    }
+
     void Start()
     {
-        
+        StartCoroutine(ARGPSFunction());
     }
 
     // Update is called once per frame
@@ -15,4 +31,48 @@ public class NewTestCoords : MonoBehaviour
     {
         
     }
+    
+    private IEnumerator ARGPSFunction()
+    {
+
+        if (!Input.location.isEnabledByUser)
+        {
+            Permission.RequestUserPermission(Permission.FineLocation);
+        }
+
+        Input.location.Start();
+        int maxWait = 5;
+
+        if(Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+        {
+            yield return new WaitForSeconds(1);
+            Mathf.Round(maxWait--);
+        }
+
+        if (maxWait < 0 )
+        {
+            Loc1Text.text = "Timed out!";
+            yield break;
+        }
+
+        if(Input.location.status == LocationServiceStatus.Failed)
+        {
+            Loc1Text.text = "Cannot determine device Location";
+            yield break;
+        }
+        else
+        {
+            #region location1Calc
+            double latLoc1 = 51.923460;
+            double lonLoc1 = 4.481160;
+            Vector2 Loc1Coords = new Vector2((float)latLoc1, (float)lonLoc1);
+            string textCoordsLoc1 = GPSEncoder.GPSToUCS(Loc1Coords).ToString();
+            Loc1Text.text = textCoordsLoc1;
+            #endregion
+            CurrentCoordText.text = $"Latitude: {Input.location.lastData.latitude} Longitude: {Input.location.lastData.longitude}";
+            Input.location.Stop();
+        }
+
+    }
+
 }
