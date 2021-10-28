6bf6db4ba532e70e11054e0f913db10648deb37a
Githubjael <jaelimamdi@gmail.com>
2021-10-28 12:00:21 +0200

    Bruh

--- ARWandelingProject/Assets/Recources/CreatedScripts/LightEstimation.cs
+++ ARWandelingProject/Assets/Recources/CreatedScripts/LightEstimation.cs
@@ -16,4 +16,42 @@ public class LightEstimation : MonoBehaviour
     private TextMeshProUGUI tempValue;
     [SerializeField]
     private TextMeshProUGUI colorCorrectionValue;
+
+    private Light currentLight;
+
+    void Awake()
+    {
+        currentLight.GetComponent<Light>();
+    }
+
+    private void OnEnable()
+    {
+        arCameraManager.frameReceived += FrameUpdated;
+    }
+
+    private void OnDisable()
+    {
+        arCameraManager.frameReceived -= FrameUpdated;
+    }
+
+    private void FrameUpdated(ARCameraFrameEventArgs args)
+    {
+        if (args.lightEstimation.averageBrightness.HasValue)
+        {
+            brightnessValue.text = $"Brightness: {args.lightEstimation.averageBrightness.Value}";
+            currentLight.intensity = args.lightEstimation.averageBrightness.Value;
+        }
+
+        if (args.lightEstimation.averageColorTemperature.HasValue)
+        {
+            tempValue.text = $"Temp Color: {args.lightEstimation.averageColorTemperature.Value}";
+            currentLight.colorTemperature = args.lightEstimation.averageColorTemperature.Value;
+        }
+
+        if (args.lightEstimation.colorCorrection.HasValue)
+        {
+            colorCorrectionValue.text = $"ColorCorrection: {args.lightEstimation.colorCorrection.Value}";
+            currentLight.color = args.lightEstimation.colorCorrection.Value;
+        }
+    }
 }
