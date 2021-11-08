57a379b153d221bae1548d81e92aee48b742bf6c
Githubjael <jaelimamdi@gmail.com>
2021-11-08 15:12:56 +0100

    retry from the beginning

--- ARWandelingProject/Assets/Recources/CreatedScripts/GazeTest.cs
+++ ARWandelingProject/Assets/Recources/CreatedScripts/GazeTest.cs
@@ -5,6 +5,9 @@ using TMPro;
 
 public class GazeTest : MonoBehaviour, InformationInterface
 {
+    /// <summary>
+    /// This script is for handeling interactions with objects through gazing at them
+    /// </summary>
     //get a curssor, we plan on putting this cursor in the middle of the screen
     [SerializeField]
     private GameObject Cursor;
@@ -18,34 +21,15 @@ public class GazeTest : MonoBehaviour, InformationInterface
     [SerializeField]
     private TextMeshProUGUI information;
 
-    public void FixedUpdate()
+    public void Start()
     {
         Gaze();
     }
     public void Gaze()
     {
-        Ray ray = new Ray(Observer.transform.position, Observer.transform.forward);
-        RaycastHit hitInfo;
-
-        if (Physics.Raycast(ray, out hitInfo, 100))
+        if (Input.GetMouseButtonDown(0) || Input.touchCount > 0 && TouchPhase.Began == 0)
         {
-            Debug.DrawLine(ray.origin, hitInfo.point, Color.green);
-        }
-        else
-        {
-            Debug.DrawLine(ray.origin, ray.origin + ray.direction * 100, Color.red);
-        }
-        Debug.DrawRay(ray.origin, hitInfo.point, Color.grey);
-        Debug.Log($"{hitInfo.collider.tag}");
-        if (hitInfo.collider.tag == "TestTag1")
-        {
-            Debug.Log("Hello!");
             infoPrompt.SetActive(true);
-            FillInText("What is up Jared?");
-        }
-        if(hitInfo.collider.tag == "Untagged")
-        {
-            infoPrompt.SetActive(false);
         }
     }
 
