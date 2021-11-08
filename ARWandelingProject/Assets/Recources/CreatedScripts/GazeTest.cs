e16d110dbc8fad729feee88a8cf394c39f443f83
Githubjael <jaelimamdi@gmail.com>
2021-11-08 15:52:39 +0100

    Update GazeTest.cs

--- ARWandelingProject/Assets/Recources/CreatedScripts/GazeTest.cs
+++ ARWandelingProject/Assets/Recources/CreatedScripts/GazeTest.cs
@@ -27,7 +27,7 @@ public class GazeTest : MonoBehaviour, InformationInterface
     }
     public void Gaze()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonUp(0))
         {
             infoPrompt.SetActive(true);
             FillInText("Die on this hill.");
