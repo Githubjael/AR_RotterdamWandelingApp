18c57df053388ef9edf3ec28574ca4ac0b88caae
Githubjael <jaelimamdi@gmail.com>
2021-11-09 12:04:19 +0100

    Update ARCursor.cs

--- ARWandelingProject/Assets/Recources/CreatedScripts/ARCursor.cs
+++ ARWandelingProject/Assets/Recources/CreatedScripts/ARCursor.cs
@@ -1,8 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
 
 public class ARCursor : MonoBehaviour
 {
-
+    [SerializeField]
+    private GameObject childObjectCursor;
+    [SerializeField]
+    private GameObject objectToPlace;
+    [SerializeField]
+    private ARRaycastManager arRaycastManager;
 }
