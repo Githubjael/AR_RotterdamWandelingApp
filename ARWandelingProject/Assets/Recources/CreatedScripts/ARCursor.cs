3aa2d3a0788400ad64a81ad39043f787b6115a0d
Githubjael <jaelimamdi@gmail.com>
2021-11-09 12:21:03 +0100

    Update ARCursor.cs

--- ARWandelingProject/Assets/Recources/CreatedScripts/ARCursor.cs
+++ ARWandelingProject/Assets/Recources/CreatedScripts/ARCursor.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,8 +8,38 @@ public class ARCursor : MonoBehaviour
 {
     [SerializeField]
     private GameObject childObjectCursor;
-    [SerializeField]
-    private GameObject objectToPlace;
+    /*
+     *    [SerializeField]
+     *    private GameObject objectToPlace;
+     */
     [SerializeField]
     private ARRaycastManager arRaycastManager;
+
+    private bool useCursor = true;
+
+    public void Start()
+    {
+        childObjectCursor.SetActive(useCursor);
+    }
+
+    public void Update()
+    {
+        if (useCursor)
+        {
+            UpdateCursor();
+        }
+    }
+
+    private void UpdateCursor()
+    {
+        Vector2 ScreenPosition = Camera.main.ViewportToScreenPoint(new Vector2(0.5f, 0.5f));
+        List<ARRaycastHit> hits = new List<ARRaycastHit>();
+        arRaycastManager.Raycast(ScreenPosition, hits, UnityEngine.XR.ARSubsystems.TrackableType.Planes);
+
+        if(hits.Count > 0)
+        {
+            transform.position = hits[0].pose.position;
+            transform.rotation = hits[0].pose.rotation;
+        }
+    }
 }
