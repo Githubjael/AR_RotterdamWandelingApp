62ca234c38f9bc2f5e0d0e2cfa704ccd33a17d43
Githubjael <jaelimamdi@gmail.com>
2021-11-09 15:51:35 +0100

    Idk what happened
    Mom?

---
+++ ARWandelingProject/Assets/Recources/CreatedScripts/CameraScripts/Gazing.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using TMPro;
+
+public class Gazing : MonoBehaviour
+{
+    [SerializeField]
+    private Camera arCamera;
+    [SerializeField]
+    private ARRaycastManager arRaycastManager;
+    [SerializeField]
+    public TextMeshProUGUI dynamicText;
+
+    public void Start()
+    {
+        
+    }
+
+    public void Update()
+    {
+        
+    }
+
+    public void Gaze()
+    {
+
+    }
+}
