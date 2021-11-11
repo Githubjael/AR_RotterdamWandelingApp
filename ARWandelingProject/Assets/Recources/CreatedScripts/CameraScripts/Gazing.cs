a877f92e1ff60e5fd101ac5f59dcf6b5d2ccc52e
Githubjael <jaelimamdi@gmail.com>
2021-11-11 13:44:40 +0100

    Hey guess what
    Turns ou you need to put raycastin in Update() instead of Start().

--- ARWandelingProject/Assets/Recources/CreatedScripts/CameraScripts/Gazing.cs
+++ ARWandelingProject/Assets/Recources/CreatedScripts/CameraScripts/Gazing.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Gazing : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private Camera arCamera;
+
+    [SerializeField]
+    private GameObject Observer;
 
     // Update is called once per frame
     void Update()
     {
-        
+        Ray ray = new Ray(arCamera.transform.position, arCamera.transform.forward);
+        RaycastHit hitsInfo;
+
+        if (Physics.Raycast(ray , out hitsInfo))
+        {
+            Debug.DrawLine(ray.origin, hitsInfo.point);
+        }
+        else
+        {
+            Debug.DrawLine(ray.origin, ray.origin + ray.direction * 100f, Color.white);
+        }
     }
 }
