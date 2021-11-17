bef3047c0fdd98b3cc1c6c10a126a82f6e9adb98
Githubjael <jaelimamdi@gmail.com>
2021-11-17 16:54:06 +0100

    bye

--- ARWandelingProject/Assets/Recources/CreatedScripts/CoordScripts/CoordToRealLocation.cs
+++ ARWandelingProject/Assets/Recources/CreatedScripts/CoordScripts/CoordToRealLocation.cs
@@ -10,27 +10,32 @@ public class CoordToRealLocation : MonoBehaviour, CoordInterface<float>
     public float longitude;
 
     [SerializeField]public List<GameObject> Placeholders = new List<GameObject>();
-    void CoordInterface<float>.Coords(float lat, float lon)
+/*    void CoordInterface<float>.Coords(float lat, float lon)
     {
         latitude = lat;
         longitude = lon;
         Vector2 CoordsToTranslate = new Vector2(latitude, longitude);
         Vector3 result = GPSEncoder.GPSToUCS(CoordsToTranslate);
         Debug.Log(result);
-        for (var i = 0; i < 3; i++)
-        {
-            Instantiate<GameObject>(Placeholders[i]);
-        }
+    }*/
+    private void Update()
+    {
+        
     }
 
-    /*  
     public void Coords(float lat, float lon)
     {
         latitude = lat;
         longitude = lon;
         Vector2 CoordsToTranslate = new Vector2(latitude, longitude);
         Vector3 result = GPSEncoder.GPSToUCS(CoordsToTranslate);
-
+        for (var  i = 0; i < 3; i++)
+        {
+            Instantiate<GameObject>(Placeholders[i]);
+            Placeholders[i].transform.Translate(result);
+            Instantiate<GameObject>(Placeholders[i]);
+            Placeholders[i].transform.position = result;
+        }
     }
-    */
+
 }
