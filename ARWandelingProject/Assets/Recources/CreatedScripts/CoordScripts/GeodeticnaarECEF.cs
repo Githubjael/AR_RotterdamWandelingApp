0b1a964fc685e81886ec22b07172612d643f608e
Githubjael <jaelimamdi@gmail.com>
2021-11-23 11:53:09 +0100

    bit of a fucky wucky temp standin
    I also added scale to scale stuff down. Might use the GPS encoder for the  SetLocalOrigin Function.

--- ARWandelingProject/Assets/Recources/CreatedScripts/CoordScripts/GeodeticnaarECEF.cs
+++ ARWandelingProject/Assets/Recources/CreatedScripts/CoordScripts/GeodeticnaarECEF.cs
@@ -5,15 +5,22 @@ using UnityEngine;
 public class GeodeticnaarECEF : MonoBehaviour
 {
     private float N = 6399594;
+    private float scale = 10000;
     [SerializeField] private float altitude;
     [SerializeField] private float latitude;
     [SerializeField] private float longitude;
-    public void Converter(float lat, float alt, float lon)
+    [SerializeField] private GameObject Placeholder;
+    public void Converter()
     {
         var X = (N + altitude) * Mathf.Cos(latitude) * Mathf.Cos(longitude);
-        var Y = (N + altitude) * Mathf.Cos(latitude) * Mathf.Sin(longitude);
-        var Z = 0;
+        var Y = 0;
+        var Z = (N + altitude) * Mathf.Cos(latitude) * Mathf.Sin(longitude);
         
-        Vector3 ConvertedXYZ = new Vector3(X, Y, Z);
+        Vector3 ConvertedXYZ = new Vector3(X / scale, Y / scale, Z / scale);
+        Instantiate(Placeholder, ConvertedXYZ, Quaternion.identity);
+    }
+    public void Start()
+    {
+        Converter();
     }
 }
