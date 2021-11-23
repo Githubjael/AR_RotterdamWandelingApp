a5854847d2264bf784c2d9e7aab34758a7ba23cf
Githubjael <jaelimamdi@gmail.com>
2021-11-23 13:41:13 +0100

    Update GeodeticnaarECEF.cs

--- ARWandelingProject/Assets/Recources/CreatedScripts/CoordScripts/GeodeticnaarECEF.cs
+++ ARWandelingProject/Assets/Recources/CreatedScripts/CoordScripts/GeodeticnaarECEF.cs
@@ -13,8 +13,11 @@ public class GeodeticnaarECEF : MonoBehaviour
     public void Converter()
     {
         var X = (N + altitude) * Mathf.Cos(latitude) * Mathf.Cos(longitude);
+        Debug.Log(X);
         var Y = 0;
+        Debug.Log(Y);
         var Z = (N + altitude) * Mathf.Cos(latitude) * Mathf.Sin(longitude);
+        Debug.Log(Z);
         
         Vector3 ConvertedXYZ = new Vector3(X / scale, Y / scale, Z / scale);
         Instantiate(Placeholder, ConvertedXYZ, Quaternion.identity);
