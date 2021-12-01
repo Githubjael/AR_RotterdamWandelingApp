1090963a64f71a6a451894742350bb9da0405401
Githubjael <jaelimamdi@gmail.com>
2021-12-01 14:18:37 +0100

    Update OntvangInput.cs

--- ARWandelingProject/Assets/Resources/Scripts/OntvangInput.cs
+++ ARWandelingProject/Assets/Resources/Scripts/OntvangInput.cs
@@ -7,21 +7,20 @@ using UnityEngine.UI;
 public class OntvangInput : MonoBehaviour
 {
     public Text LATLON;
-    public void Start()
+    public void Update()
     {
-        
+        Receiver();
     }
     public void Receiver()
     {
         if (!Permission.HasUserAuthorizedPermission(Permission.FineLocation))
         {
             Permission.RequestUserPermission(Permission.FineLocation);
+            Input.location.Start();
         }
-
-        Input.location.Start();
+        
         string lat = Input.location.lastData.latitude.ToString();
         string lon = Input.location.lastData.longitude.ToString();
         LATLON.text = $"lat:{lat}, lon:{lon}";
-        Input.location.Stop();
     }
 }
