28677d084b9ee0e848148c661783bdc4edf0beb2
Githubjael <jaelimamdi@gmail.com>
2021-12-01 14:17:52 +0100

    scripting test
    ik moest een kleine ui maken maar ik ga er niet echt op focussen.

--- ARWandelingProject/Assets/Resources/Scripts/OntvangInput.cs
+++ ARWandelingProject/Assets/Resources/Scripts/OntvangInput.cs
@@ -1,11 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Android;
+using UnityEngine.UI;
 
 public class OntvangInput : MonoBehaviour
 {
+    public Text LATLON;
+    public void Start()
+    {
+        
+    }
     public void Receiver()
     {
+        if (!Permission.HasUserAuthorizedPermission(Permission.FineLocation))
+        {
+            Permission.RequestUserPermission(Permission.FineLocation);
+        }
 
+        Input.location.Start();
+        string lat = Input.location.lastData.latitude.ToString();
+        string lon = Input.location.lastData.longitude.ToString();
+        LATLON.text = $"lat:{lat}, lon:{lon}";
+        Input.location.Stop();
     }
 }
