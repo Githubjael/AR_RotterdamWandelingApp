8fa9e718653e0a7a09aec4d69db65f3a0f514ee5
Githubjael <jaelimamdi@gmail.com>
2021-12-01 15:18:56 +0100

    Het location receiver werkt ook!

--- ARWandelingProject/Assets/Resources/Scripts/OntvangInput.cs
+++ ARWandelingProject/Assets/Resources/Scripts/OntvangInput.cs
@@ -7,20 +7,38 @@ using UnityEngine.UI;
 public class OntvangInput : MonoBehaviour
 {
     public Text LATLON;
-    public void Update()
+    public void Start()
     {
-        Receiver();
+        StartCoroutine(Receiver());
     }
-    public void Receiver()
+    public IEnumerator Receiver()
     {
+        //ask for permission if there is none, just in case.
         if (!Permission.HasUserAuthorizedPermission(Permission.FineLocation))
         {
             Permission.RequestUserPermission(Permission.FineLocation);
-            Input.location.Start();
         }
-        
-        string lat = Input.location.lastData.latitude.ToString();
-        string lon = Input.location.lastData.longitude.ToString();
-        LATLON.text = $"lat:{lat}, lon:{lon}";
+        Input.location.Start();
+        int maxWait = 5;
+        if( Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+        {
+            yield return new WaitForSeconds(1);
+            maxWait--;
+        }
+        if(maxWait < 0)
+        {
+            yield break;
+        }
+        if(Input.location.status == LocationServiceStatus.Failed)
+        {
+            yield break;
+        }
+        else
+        {
+            string lat = Input.location.lastData.latitude.ToString();
+            string lon = Input.location.lastData.longitude.ToString();
+            LATLON.text = $"lat:{lat}, lon:{lon}";
+        }
+        Input.location.Stop();
     }
 }
