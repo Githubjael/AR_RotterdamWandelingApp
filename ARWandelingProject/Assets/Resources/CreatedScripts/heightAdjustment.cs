bf7da15bc1b4de4d706a91ff40f89b72b6d3d290
Githubjael <jaelimamdi@gmail.com>
2021-12-14 11:22:23 +0100

    stuffs

--- ARWandelingProject/Assets/Resources/CreatedScripts/heightAdjustment.cs
+++ ARWandelingProject/Assets/Resources/CreatedScripts/heightAdjustment.cs
@@ -7,7 +7,6 @@ using UnityEngine.UI;
 public class heightAdjustment : MonoBehaviour
 {
     public Text heightInput;
-    // Start is called before the first frame update
 
     public void function()
     {
