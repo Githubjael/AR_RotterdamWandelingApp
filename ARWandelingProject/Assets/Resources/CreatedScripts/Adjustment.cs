7aff60ad19343b8b14794346b0d242ceea351376
Githubjael <jaelimamdi@gmail.com>
2021-12-14 16:54:30 +0100

    imandoomed

---
+++ ARWandelingProject/Assets/Resources/CreatedScripts/Adjustment.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+public class Adjustment : MonoBehaviour
+{
+    [SerializeField]private ARSessionOrigin sessionOrigin;
+    public GameObject Content;
+    public int i = 0;
+    public GameObject inputfield;
+    public Camera Camera;
+
+    public void Start()
+    {
+        sessionOrigin = FindObjectOfType<ARSessionOrigin>();
+        //Camera = GetComponent<Camera>();
+    }
+
+    public void heightAdjustmentUP()
+    {
+        i++;
+        Camera.transform.position = new Vector3(0, i, 0);
+    }
+    public void heightAdjustmentDOWN()
+    {
+        i--;
+        Camera.transform.position = new Vector3(0, i, 0);
+    }
+    public void OnvalueChange(float value)
+    {
+        Debug.Log("New Value:" + value);
+        //Transform t = gameObject.transform;
+        sessionOrigin.MakeContentAppearAt(Content.transform, Quaternion.identity);
+
+        Content.transform.localScale = new Vector3(value, value, value);
+    }
+}
