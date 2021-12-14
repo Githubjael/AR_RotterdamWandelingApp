73b723b11611c77cfeb5ccca420ac80f03c268a8
Githubjael <jaelimamdi@gmail.com>
2021-12-14 11:39:36 +0100

    beforetest

--- ARWandelingProject/Assets/Resources/CreatedScripts/heightAdjustment.cs
+++ ARWandelingProject/Assets/Resources/CreatedScripts/heightAdjustment.cs
@@ -3,15 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 
 public class heightAdjustment : MonoBehaviour
 {
     public Text heightInput;
+    public Camera Camera;
 
     public void function()
     {
-        int i = Convert.ToInt32(heightInput);
-        var position = gameObject.transform.position;
+        int i = Convert.ToInt16(heightInput.text);
+        var position = Camera.transform.position;
         position.y = i;
     }
 }
