c7bf64bfa9832d0782de4d73cfff12b332a39f41
Githubjael <jaelimamdi@gmail.com>
2022-01-10 16:48:50 +0100

    Update EquirectangularProjection.cs

--- ARWandelingProject/Assets/Resources/CreatedScripts/EquirectangularProjection.cs
+++ ARWandelingProject/Assets/Resources/CreatedScripts/EquirectangularProjection.cs
@@ -4,5 +4,23 @@ using UnityEngine;
 
 public class EquirectangularProjection : MonoBehaviour
 {
+    public float Radius = 6371 * 1000;
+    public float longitude;
+    public float latitude;
 
+    public float central_meridian;
+
+
+    //public void 
+    public float x_vector()
+    {
+        var x = Radius * (longitude / central_meridian) * Mathf.Cos(central_parallel);
+        return x;
+    }
+
+    public float y_vector()
+    {
+        var y = Radius * (latitude / central_parallel);
+        return y;
+    }
 }
