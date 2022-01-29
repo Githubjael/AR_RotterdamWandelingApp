132750a254601b5502ab5cdd45f01a35cc9c10f3
Githubjael <jaelimamdi@gmail.com>
2022-01-29 02:23:19 +0100

    Start
    om script te maken.

---
+++ ARWandelingProject/Assets/Resources/CreatedScripts/SightLines.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SightLines : MonoBehaviour
+{
+    public Ray ray;
+    public RaycastHit hitinfo;
+    public Camera arCamera;
+    public Transform player;
+
+    void SightSeeing()
+    {
+        var playerPosition = player.transform.position;
+        /*if ()
+        {
+        hier moet nog iets ingevuld worden, leeg laten voor nu
+        }*/
+    }
+
+    void LoreInteraction()
+    {
+
+    }
+}
