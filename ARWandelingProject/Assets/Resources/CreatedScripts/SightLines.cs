8e167e9b8312ec4f99af558ee88c2408031f3e19
Githubjael <jaelimamdi@gmail.com>
2022-01-31 16:12:39 +0100

    ui cursor stuff

--- ARWandelingProject/Assets/Resources/CreatedScripts/SightLines.cs
+++ ARWandelingProject/Assets/Resources/CreatedScripts/SightLines.cs
@@ -1,21 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SightLines : MonoBehaviour
 {
-    public Ray ray;
-    public RaycastHit hitinfo;
-    public Camera arCamera;
     public Transform player;
+    public Camera arCamera;
+    public RectTransform transformCursor;
+    public float smoothValue = 2f;
+
+    public void Update()
+    {
+        SightSeeing();
+    }
 
     void SightSeeing()
     {
-        var playerPosition = player.transform.position;
-        /*if ()
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+        RaycastHit hitInfo;
+        Ray ray = new Ray(transform.position, fwd);
+
+        Debug.DrawLine(transform.position, transform.position + fwd * 10f, Color.red);
+        if (Physics.Raycast(transform.position, fwd, out hitInfo))
         {
-        hier moet nog iets ingevuld worden, leeg laten voor nu
-        }*/
+            Debug.Log(hitInfo.collider.name);
+            /*transformCursor.transform.localScale = new Vector3(0.75f, 0.75f, 0)*/
+            var cursorScale = transformCursor.transform.localScale;
+/*            cursorScale = Vector3.Lerp(cursorScale.x, , Time.fixedDeltaTime * smoothValue);
+*/        }
+        else
+        {
+            transformCursor.transform.localScale = new Vector3(-0.25f, -0.25f, 0);
+        }
     }
 
     void LoreInteraction()
