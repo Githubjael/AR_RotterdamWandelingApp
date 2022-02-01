42e75f36852687422d155189773d20c8be572781
Githubjael <jaelimamdi@gmail.com>
2022-02-01 09:56:20 +0100

    Update SightLines.cs

--- ARWandelingProject/Assets/Resources/CreatedScripts/SightLines.cs
+++ ARWandelingProject/Assets/Resources/CreatedScripts/SightLines.cs
@@ -1,7 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class SightLines : MonoBehaviour
 {
@@ -12,37 +9,18 @@ public class SightLines : MonoBehaviour
     private RectTransform transformCursor;
 
     [SerializeField]
+    private float startScale = 0.25f;
+
+    [SerializeField]
+    private float endScale = 0.75f;
+
+    [SerializeField]
     [Range(0f, 5f)]
     private float smoothValue = 2f;
 
-    public void Update()
-    {
-        SightSeeing();
-    }
+    [SerializeField]
+    [Range(0.25f, 0.75f)]
+    private float size;
 
-    void SightSeeing()
-    {
-        Vector3 fwd = transform.TransformDirection(Vector3.forward).normalized;
-        Ray ray = new Ray(transform.position, fwd);
-        RaycastHit hitInfo;
 
-        Debug.DrawLine(transform.position, transform.position + fwd * 10f, Color.red);
-
-        if (Physics.Raycast(transform.position, fwd, out hitInfo))
-        {
-            Debug.Log(hitInfo.collider.name);
-            /*transformCursor.transform.localScale = new Vector3(0.75f, 0.75f, 0)*/
-            var cursorScale = transformCursor.transform.localScale;
-            /*cursorScale = Vector3.Lerp(cursorScale.x, , Time.fixedDeltaTime * smoothValue);*/
-        }
-        else
-        {
-            transformCursor.transform.localScale = new Vector3(0.25f, 0.25f, 0);
-        }
-    }
-
-    void LoreInteraction()
-    {
-
-    }
 }
