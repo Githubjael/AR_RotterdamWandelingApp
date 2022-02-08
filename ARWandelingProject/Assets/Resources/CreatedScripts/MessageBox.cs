b5134941fff3cc60060fdcdc303922556a7c911a
Githubjael <jaelimamdi@gmail.com>
2022-02-08 12:46:37 +0100

    im going to make messge boxes

---
+++ ARWandelingProject/Assets/Resources/CreatedScripts/MessageBox.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor.UI;
+using TMPro;
+
+public class MessageBox : MonoBehaviour
+{
+    TextMeshProUGUI header;
+    TextMeshProUGUI textBox;
+    public void Start()
+    {
+        
+    }
+
+    public void DynamicText()
+    {
+
+    }
+}
