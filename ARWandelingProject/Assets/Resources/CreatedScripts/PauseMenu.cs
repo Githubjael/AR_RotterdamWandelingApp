2215730be727c9bfd2eed316ddec3351fe75d069
Githubjael <jaelimamdi@gmail.com>
2022-02-09 12:07:53 +0100

    Pausestuff
    code updates.

--- ARWandelingProject/Assets/Resources/CreatedScripts/PauseMenu.cs
+++ ARWandelingProject/Assets/Resources/CreatedScripts/PauseMenu.cs
@@ -1,18 +1,33 @@
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public string MenuScene = "2Maps";
+    public static bool GameIsPaused = false;
+    public GameObject PauseMenuUI;
 
     // Update is called once per frame
-    void Update()
+    public void Resume()
     {
-        
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+    public void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene(MenuScene);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
