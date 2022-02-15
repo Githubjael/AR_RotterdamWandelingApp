89876f8dcbd8eee50068e07de8f00f68772392e6
Githubjael <jaelimamdi@gmail.com>
2022-02-15 14:16:59 +0100

    nieuwe script na testen en herschrijven.
    Dit werkt heel goed! nou de resultaat is echt één van een prototype, maar dat is het ook.

--- ARWandelingProject/Assets/Resources/CreatedScripts/LocationInfoPanel.cs
+++ ARWandelingProject/Assets/Resources/CreatedScripts/LocationInfoPanel.cs
@@ -1,20 +1,49 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.Audio;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
 public class LocationInfoPanel : MonoBehaviour
 {
     public ResponsiveReticle responsiveReticle;
+    public Transform LocationInfoTexts;
 
-    private void Start()
+    public GameObject ListOfTexts;
+
+    public List<RectTransform> listOfPanels;
+
+    public void Start()
+    {
+        ListChildren();
+    }
+
+    public void Update()
     {
         OpenInfoPanel();
     }
+
     public void OpenInfoPanel()
     {
         if (responsiveReticle.isInteractable)
         {
             Debug.Log("Ey!");
+            ListOfTexts.SetActive(true);
         }
+        else
+        {
+            ListOfTexts.SetActive(false);
+        }
+    }
+
+    public void ListChildren()
+    {
+        
+        foreach(RectTransform child in LocationInfoTexts)
+        {
+            Debug.Log("Child:" + child.name);
+            listOfPanels.Add(child);
+        }
+        listOfPanels[1].gameObject.SetActive(true);
     }
 }
