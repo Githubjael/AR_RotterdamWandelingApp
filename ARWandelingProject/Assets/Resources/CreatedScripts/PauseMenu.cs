a24924333728767719bc7a2b6b65a4b01f0e59d8
Githubjael <jaelimamdi@gmail.com>
2022-02-21 15:27:55 +0100

    script scale & huis model

--- ARWandelingProject/Assets/Resources/CreatedScripts/PauseMenu.cs
+++ ARWandelingProject/Assets/Resources/CreatedScripts/PauseMenu.cs
@@ -1,8 +1,9 @@
 using UnityEngine;
+using UnityEngine.Audio;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.XR.ARFoundation;
 using UnityEngine.SceneManagement;
-using UnityEngine.Audio;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -11,44 +12,59 @@ public class PauseMenu : MonoBehaviour
     public static bool GameIsPaused = false;
     public GameObject PauseMenuUI;
     public GameObject SettingsMenuUI;
+    public ARSessionOrigin referenceToSessionOrigin;
+    public Transform t;
+    public GameObject objectsToScale;
 
-    // Update is called once per frame
+    //zet de options panel op scherm
     public void Resume()
     {
         PauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         GameIsPaused = false;
     }
+    //zet de options menu op de scherm
     public void Pause()
     {
         PauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         GameIsPaused = true;
     }
+    //deze gaat naar de main menu
     public void LoadMenu()
     {
         SceneManager.LoadScene(MenuScene);
     }
+    //eindigt de eel applicatie
     public void QuitGame()
     {
         Application.Quit();
     }
+    //zet de settings op de scherm
     public void LoadSettings()
     {
         SettingsMenuUI.SetActive(true);
         PauseMenuUI.SetActive(false);
     }
+    //haalt settings weg
     public void DeloadSettings()
     {
         SettingsMenuUI.SetActive(false);
         PauseMenuUI.SetActive(true);
     }
+    //functie om Volume te zetten
     public void SetVolume(float volume)
     {
         audioMixer.SetFloat("Volume", volume);
     }
+    //functie om Graphic quality te zetten
     public void SetQuality(int qualityIndex)
     {
         QualitySettings.SetQualityLevel(qualityIndex);
+    }
+    public void OnValueChange(float value)
+    {
+        referenceToSessionOrigin.MakeContentAppearAt(objectsToScale.transform, Quaternion.identity);
+        objectsToScale.transform.localScale = new Vector3(value, value, value);
     }
 }
