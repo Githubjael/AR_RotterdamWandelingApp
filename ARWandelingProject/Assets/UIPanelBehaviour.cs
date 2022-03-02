89d7aa5bf2706f06e7996440ecee77d510e791d2
Githubjael <jaelimamdi@gmail.com>
2022-03-02 10:40:53 +0100

    Update UIPanelBehaviour.cs
    cooment voor de zekrheid.

--- ARWandelingProject/Assets/UIPanelBehaviour.cs
+++ ARWandelingProject/Assets/UIPanelBehaviour.cs
@@ -4,5 +4,9 @@ using System.Collections.Generic;
 
 public class UIPanelBehaviour : MonoBehaviour
 {
-
+    /*
+     * wanneer de reticle op de cube is en het object is interactable dan opent het de respectief UI Panel voor die cube:
+     * cube in LOC1, opent loc1 panel
+     */
+    
 }
