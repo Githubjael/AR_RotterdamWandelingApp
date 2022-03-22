eefa5ecae5fe01ebe640bcb60e85bf713d5b3158
Githubjael <jaelimamdi@gmail.com>
2022-03-22 14:21:27 +0100

    script update en ik ga testen
    ik heb ook in de editor de texts objects ingevuld.

--- ARWandelingProject/Assets/Resources/CreatedScripts/TextApplicator.cs
+++ ARWandelingProject/Assets/Resources/CreatedScripts/TextApplicator.cs
@@ -2,18 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using TMPro;
+
 public class TextApplicator : MonoBehaviour
 {
     [SerializeField] TextAsset textfile;
     [SerializeField] string[] tests;
     [SerializeField] GameObject[] UIToFill;
+    [SerializeField] TMP_Text[] textsToFill;
     private void Awake()
     {
         ReadFile();
+        ApplyText();
     }
 
     void ReadFile()
     {
         tests = textfile.text.Split(new string[] {",", "\n" }, StringSplitOptions.None);
+    }
+
+    void ApplyText()
+    {
+        for (int i = 0; i < textsToFill.Length; i++)
+        {
+            textsToFill[i].text = tests[i];
+        }
     }
 }
