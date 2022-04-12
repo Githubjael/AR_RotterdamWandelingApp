f39e976c1abb38f92ebeae6c002bed454b33f263
Githubjael <jaelimamdi@gmail.com>
2022-04-12 16:46:58 +0200

    ik heb nog een text applicator gemaakt
    deze is voor mobile.

---
+++ ARWandelingProject/Assets/Resources/CreatedScripts/ThirdTextApplicator.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using System.Collections;
+using System.Text.RegularExpressions;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ThirdTextApplicator : MonoBehaviour
+{
+    [SerializeField] Transform UIPanel;
+    string fileText;
+    [SerializeField] GameObject UITextObject;
+    private void OnEnable()
+    {
+        ReadTextfiles();
+        DisplayText();
+    }
+
+    void ReadTextfiles()
+    {
+        fileText = Resources.Load<TextAsset>("CreatedTextAssets/wandelingTest 1").text;
+    }
+
+    void DisplayText()
+    {
+        UITextObject.GetComponent<Text>().text = fileText;
+        Instantiate(UITextObject, UIPanel);
+    }
+}
