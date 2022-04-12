9a134d5f241e6a2a1e771b6b4a6bb3cb78f5d20f
Githubjael <jaelimamdi@gmail.com>
2022-04-12 11:13:12 +0200

    Squashed commit of the following:
    commit 03d68c792d97062d152d8965e1453eb946d90c24
    Author: Githubjael <jaelimamdi@gmail.com>
    Date:   Tue Apr 12 11:03:13 2022 +0200
    
        ik ga deze branches te mergen
    
        hopelijk gaat wel goed gaan.
    
    commit 2e2a04f0ba5b7a5c98392f7c84c0f6d5fe1f2a9f
    Author: Githubjael <jaelimamdi@gmail.com>
    Date:   Tue Apr 12 10:45:09 2022 +0200
    
        UIPanelInfo string
    
        Ik heb veranderingen gemaakt. naar de other text application en ik ben van plan om het meer dynamisch te maken. Het is nu te hard-coded.
    
    commit 1cd6ae55e16678e4e71f132b6dff1392e2fd2d53
    Author: Githubjael <jaelimamdi@gmail.com>
    Date:   Mon Apr 11 16:41:54 2022 +0200
    
        i want free stuff

---
+++ ARWandelingProject/Assets/Resources/CreatedScripts/OtherTextApplicator.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+using System.Linq;
+using System.IO;
+
+public class OtherTextApplicator : MonoBehaviour
+{
+    [SerializeField] Transform UIPanelForText;
+    [SerializeField] GameObject UIToString;
+    [SerializeField] List<string> fileLines;
+
+    public void Awake()
+    {
+        ReadFileText();
+        DisplayUI();
+    }
+    public void ReadFileText()
+    {
+        string readFromFilePath = Application.streamingAssetsPath + "/Loc1/" + "wandelingTest" + ".txt";
+
+        fileLines = File.ReadAllLines(readFromFilePath).ToList();
+    }
+
+    public void DisplayUI()
+    {
+        foreach(string line in fileLines)
+        {
+            Instantiate(UIToString, UIPanelForText);
+            UIToString.GetComponent<Text>().text = line;
+        }
+    }
+}
