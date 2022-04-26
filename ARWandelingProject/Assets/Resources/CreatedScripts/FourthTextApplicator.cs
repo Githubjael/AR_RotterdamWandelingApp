07ebf197aa7b03718f5e126cd59ff4ea3691ee71
Githubjael <jaelimamdi@gmail.com>
2022-04-26 13:25:56 +0200

    stop ik ga het in een ander branch. verder
    ik moet objecten meer modifiable maken.

--- ARWandelingProject/Assets/Resources/CreatedScripts/FourthTextApplicator.cs
+++ ARWandelingProject/Assets/Resources/CreatedScripts/FourthTextApplicator.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Collections;
 using UnityEngine.UI;
 using UnityEngine;
+using System.IO;
 using TMPro;
+using System.Text;
 
 public class FourthTextApplicator : MonoBehaviour
 {
     [SerializeField] TextAsset[] TextFiles;
+    [SerializeField] TMP_Text[] UIPanelHeaderText;
     [SerializeField] TMP_Text[] UIPanelText;
 
     private void OnEnable()
@@ -18,9 +21,14 @@ public class FourthTextApplicator : MonoBehaviour
     void ReadAndDisplay()
     {
         for(int i = 0; i < TextFiles.Length; i++)
-        {
+        {   
             string textFromFiles = TextFiles[i].text;
             UIPanelText[i].text = textFromFiles;
+            string[] headerTextFromFiles = File.ReadAllLines("Assets/Resources/CreatedTextAssets/.txt", Encoding.UTF8);
+            foreach(string headerText in headerTextFromFiles)
+            {
+                UIPanelHeaderText[i].text = headerText;
+            }
         }
     }
 }
